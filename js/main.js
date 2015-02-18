1f5b43d6cc88aa9a3bebbf12091b308c178b2e7e
Joey Kudish <joachim.kudish@gmail.com>
2015-02-18 12:56:36 +1100

    load drawing plugin and adjust the initial view

--- js/main.js
+++ js/main.js
@@ -4,16 +4,27 @@ var map = L.map('map', {
 	maxZoom: mapMaxZoom,
 	minZoom: mapMinZoom,
 	crs: L.CRS.Simple
-}).setView([0, 0], mapMaxZoom);
+}).setView([-15, 60], 5 );
 
 var mapBounds = new L.LatLngBounds(
 	map.unproject([0, 6912], mapMaxZoom),
 	map.unproject([9984, 0], mapMaxZoom));
 
-map.fitBounds(mapBounds);
 L.tileLayer('tiles/{z}/{x}/{y}.png', {
 	minZoom: mapMinZoom, maxZoom: mapMaxZoom,
 	bounds: mapBounds,
 	attribution: 'Map data &copy; ColorPlan',
 	noWrap: true
 }).addTo(map);
+
+var featureGroup = L.featureGroup().addTo(map);
+
+var drawControl = new L.Control.Draw({
+	edit: {
+		featureGroup: featureGroup
+	}
+}).addTo(map);
+
+map.on('draw:created', function(e) {
+	featureGroup.addLayer(e.layer);
+});
