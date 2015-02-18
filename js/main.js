55930a1aeb571a6e575b135d613dab0e6d72a4e9
Joey Kudish <joachim.kudish@gmail.com>
2015-02-18 16:00:49 +1100

    adjust zoom and scale for the map

--- js/main.js
+++ js/main.js
@@ -1,20 +1,20 @@
-var mapMinZoom = 0;
+var mapMinZoom = 3;
 var mapMaxZoom = 6;
 var map = L.map('map', {
 	maxZoom: mapMaxZoom,
 	minZoom: mapMinZoom,
 	crs: L.CRS.Simple
-}).setView([-15, 60], 5 );
+}).setView([-40, 80], 3 );
 
 var mapBounds = new L.LatLngBounds(
 	map.unproject([0, 6912], mapMaxZoom),
 	map.unproject([9984, 0], mapMaxZoom));
 
 L.tileLayer('tiles/{z}/{x}/{y}.png', {
-	minZoom: mapMinZoom, maxZoom: mapMaxZoom,
-	bounds: mapBounds,
+	minZoom: mapMinZoom,
+	maxZoom: mapMaxZoom,
 	attribution: 'Map data &copy; ColorPlan',
-	noWrap: true
+	bounds: mapBounds,
 	errorTileUrl: 'tiles/empty.png'
 }).addTo(map);
 
