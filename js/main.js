542cd0feea56e9ffbf55c7dfa41e66a5eed7d7a5
Joey Kudish <joachim.kudish@gmail.com>
2015-02-18 12:49:51 +1100

    init the map!

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
+var mapMinZoom = 0;
+var mapMaxZoom = 6;
+var map = L.map('map', {
+	maxZoom: mapMaxZoom,
+	minZoom: mapMinZoom,
+	crs: L.CRS.Simple
+}).setView([0, 0], mapMaxZoom);
 
+var mapBounds = new L.LatLngBounds(
+	map.unproject([0, 6912], mapMaxZoom),
+	map.unproject([9984, 0], mapMaxZoom));
+
+map.fitBounds(mapBounds);
+L.tileLayer('tiles/{z}/{x}/{y}.png', {
+	minZoom: mapMinZoom, maxZoom: mapMaxZoom,
+	bounds: mapBounds,
+	attribution: 'Map data &copy; ColorPlan',
+	noWrap: true
+}).addTo(map);
