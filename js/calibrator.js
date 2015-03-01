9c0579a46d336ff8c41a040586a041de8c9ebbcf
Joey Kudish <joachim.kudish@gmail.com>
2015-03-01 17:09:33 +1100

    mostly finished calibrator
    done for the demo at least

--- js/calibrator.js
+++ js/calibrator.js
@@ -3,33 +3,71 @@
  */
 
 L.Calibrator = L.Class.extend({
-	initialize: function (map) {
+	_calibrationDrawer : null,
+
+	addTo: function (map) {
 		this._map = map;
-		if( confirm( 'Welcome to the takeoffs app, please calibrate the scale by making a line and deciding on the scale.' ) ) {
-			var CalibrationDrawer = new L.CalibratorLine(map, {
-				showLength: false,
-				shapeOptions: {
-					color : '#40A919',
-					weight: 4,
-					opacity: 1
-				}
-			});
-			CalibrationDrawer.enable();
-		}
+
+		this._calibrationDrawer = new L.CalibratorLine( this._map, {
+			shapeOptions: {
+				color : '#40A919',
+				weight: 4,
+				opacity: 1
+			},
+			calibrator : this
+		});
+		this._calibrationDrawer.enable();
+
 		return this;
+	},
+
+	hasCalibrated : function() {
+		return this._calibrationDrawer._calibratedMeasurement > 0;
+	},
+
+	_calibratorMeasuredDistance : function() {
+		return this._calibrationDrawer._measurementRunningTotal;
+	},
+
+	_calibratorCalibratedDistance : function() {
+		return this._calibrationDrawer._calibratedMeasurement;
+	},
+
+	calculateCalibratedDistance : function( measuredDistance ) {
+		return ( measuredDistance * this._calibratorCalibratedDistance() ) / this._calibratorMeasuredDistance();
 	}
 });
 
 
-L.drawLocal.draw.handlers.polyline.tooltip.start = 'Click to begin calibration measurement';
-L.drawLocal.draw.handlers.polyline.tooltip.cont = 'Click to end calibration measurement';
+L.CalibratorLine = L.Draw.Polyline.extend({
+	_calibratedMeasurement : 0,
+	options: {
+		startText : 'Click to start calibration measurement',
+		endText : 'Click to end calibration measurement'
+	}
+});
 
-L.CalibratorLine = L.Draw.Polyline.extend({});
+L.CalibratorLine.prototype._getTooltipText = function () {
+	var labelText;
+	if ( this._markers.length === 0) {
+		labelText = {
+			text: this.options.startText
+		};
+	} else {
+		labelText = {
+			text: this.options.endText
+		};
+	}
+	return labelText;
+};
+
+
 L.CalibratorLine.prototype.addVertex = function( latlng ){
 	L.Draw.Polyline.prototype.addVertex.call( this, latlng );
 	if ( this._markers.length !== 2 )
 		return;
 
 	this._finishShape();
-	measurement = prompt( 'What\'s the distance we just measured? (in ft)' );
+	this._calibratedMeasurement = prompt( 'What\'s the distance we just measured? (in m)' );
+	L.control.scale().addTo( this._map );
 };
